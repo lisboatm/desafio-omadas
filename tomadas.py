9a41971a30ccf23d79c8875bc70a874530d24a9a
Thiago Lisboa <36263673+lisboatm@users.noreply.github.com>
2024-11-13 20:35:00 -0300

    Update tomadas.py

--- tomadas.py
+++ tomadas.py
@@ -1,10 +1,8 @@
-def max_devices(T1, T2, T3, T4):
-    # Calcula o número máximo de dispositivos que podem ser conectados
-    total = T1 + T2 + T3 + T4 - 3
-    return total
-
-# Leitura da entrada
+# Leitura dos quatro inteiros que representam as tomadas de cada régua
 T1, T2, T3, T4 = map(int, input().split())
 
+# Cálculo do número máximo de aparelhos que podem ser conectados
+max_aparelhos = T1 + T2 + T3 + T4 - 3
+
 # Exibe o resultado
-print(max_devices(T1, T2, T3, T4))
+print(max_aparelhos)
